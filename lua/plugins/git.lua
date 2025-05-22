40c578ecfcabf0608047b9e66d3b1e3dfae3640a
HAACK, FINNEGAN <finnegan.haack@lhind.dlh.de>
2025-05-22 08:47:47 +0200

    add git status

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,6 +3,7 @@ return {
 		"tpope/vim-fugitive",
 		config = function()
 			vim.keymap.set("n", "<leader>gb", ":Git blame<CR>", {})
+            vim.keymap.set("n", "<leader>gs", ":Git status<CR>", {})
 		end,
 	},
 	{
